ba5c9e50817395b8693d414ae262ed0fe078cd17
hyungjunac <whmoon00@naver.com>
2024-04-26 17:23:32 +0900

    locale 작업중

---
+++ src/i18n.js
@@ -0,0 +1,26 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import enResources from "./locales/enResources"; // 영어 리소스 파일 임포트
+import koResources from "./locales/koResources"; // 한글 리소스 파일 임포트
+
+const resources = {
+  en: enResources,
+  ko: koResources,
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'ko',
+    debug: true,
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
---
+++ src/locales/koResource.js
@@ -0,0 +1,9 @@
+const koResource = {
+    translation: {
+        login: {
+            userId:'사용자아이디',
+            password:'패스워드',
+        }
+    }
+}
+export default koResource;
