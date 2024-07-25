65c3cb9e7b2c90d610a29b6c81d30972f6d3e61b
hyungsung <hyungsung@sindoh.com>
2024-07-25 18:27:54 +0900

    [CompanyAdd] 주소 표시 문제 대응

--- src/constants/AddAddressItem.jsx
+++ src/constants/AddAddressItem.jsx
@@ -42,7 +42,7 @@ const AddAddressItem = (props) => {
                     className="add-basic-content"
                     id="add_adress_input"
                     type="text"
-                    defaultValue={edited[key_address]}
+                    value={edited[key_address]}
                     placeholder={title}
                     disabled={disabled ? disabled : false}
                     onChange={handleChange}
